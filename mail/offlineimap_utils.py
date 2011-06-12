b120bbfbcd8277d33fe90b77a539994146f1134c
Shrikant-Sharat <shrikantsharat.k@gmail.com>
2011-06-12 10:51:14 +0530

    Awesome configuration with offlineimap and mutt with ldap search
    --HG--
    rename : offlineimap.conf => mail/offlineimap.conf

---
+++ mail/offlineimap_utils.py
@@ -0,0 +1,18 @@
+
+import os
+from ConfigParser import SafeConfigParser
+
+creds = SafeConfigParser()
+creds.readfp(open(os.path.expanduser('~/Dropbox/Private/creds.ini')))
+
+def read_pass():
+    return creds.get('cmail', 'password')
+
+def read_user():
+    return creds.get('cmail', 'username')
+
+def read_host():
+    return creds.get('cmail', 'imap-server')
+
+def read_port():
+    return creds.get('cmail', 'imap-port')
