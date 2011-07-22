0e2ba902448ba5e98b739f121ee5d221908d05c0
Shrikant-Sharat <shrikantsharat.k@gmail.com>
2011-07-22 22:35:52 +0530

    Using custom parex, in common library in dotfiles repository

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,13 @@ from __future__ import print_function
 from fabric.api import *
 import fabric.colors as co
 import os, os.path as p
+from repo_cmds import get_repo_cmd
 from parex import TaskManager
 
 # Some nice plugins not included
 # - Command-T
 
-PLUGIN_REPOS = (r for r in '''
+PLUGIN_REPOS = (r.strip() for r in '''
 
 git://github.com/tpope/vim-pathogen.git
 
@@ -19,8 +20,6 @@ git://github.com/tpope/vim-surround.git
 git://github.com/tpope/vim-abolish.git
 git://github.com/mileszs/ack.vim.git
 git://github.com/vim-scripts/AutoClose.git
-git://github.com/vim-scripts/DirDiff.vim.git
-git://github.com/tpope/vim-fugitive.git
 git://github.com/michaeljsmith/vim-indent-object.git
 git://github.com/vim-scripts/matchit.zip.git
 git://github.com/tpope/vim-repeat.git
@@ -33,8 +32,14 @@ git://github.com/ehamberg/vim-cute-python.git
 git://github.com/pangloss/vim-javascript.git
 git://github.com/vim-scripts/Mark.git
 git://github.com/vim-scripts/MarkLines.git
-#git://github.com/vim-scripts/vorax.git
 git://github.com/kchmck/vim-coffee-script.git
+git://github.com/godlygeek/tabular.git
+git://github.com/vim-scripts/Conque-Shell.git
+git://github.com/vim-scripts/vimwiki.git
+git://github.com/scrooloose/nerdtree.git
+git://github.com/tpope/vim-ragtag.git
+https://bitbucket.org/sjl/gundo.vim
+git://github.com/Lokaltog/vim-easymotion.git
 
 # This one's original repo is bzr based
 git://github.com/vim-scripts/UltiSnips.git
@@ -48,31 +53,26 @@ git://github.com/vim-scripts/L9.git
 #git://github.com/vim-scripts/cvsmenu.vim-updated.git
 git://github.com/vim-scripts/vcscommand.vim.git
 
-git://github.com/scrooloose/nerdtree.git
-
 # https://sharat87@github.com/sharat87/sessionman.vim.git
 git://github.com/sharat87/sessionman.vim.git
 
 https://bitbucket.org/sharat87/vim-looks
 
-git://github.com/vim-scripts/Conque-Shell.git
-
-git://github.com/vim-scripts/vimwiki.git
-
 # have to force clone this one
 https://bitbucket.org/kotarak/vimclojure force-clone
 
-git://github.com/tpope/vim-ragtag.git
-
-https://bitbucket.org/sjl/gundo.vim
-
 git://github.com/Shougo/vimproc.git
-
 git://github.com/ujihisa/quickrun.git
 
-'''.splitlines() if r and not r.startswith('#'))
+# Converts PNG and GIF icons to XPM with ImageMagick when edited in vim
+git://github.com/tpope/vim-afterimage.git
 
-@task
+# Adds a command ":Pythondo", which is rather like ":perldo" or" ":rubydo".
+git://github.com/vim-scripts/pythondo.git
+
+'''.splitlines() if r.strip() and not r.startswith('#'))
+
+@task(default=True)
 def up():
 
     if not p.exists('bundle'):
@@ -90,6 +90,10 @@ def up():
         procs[pid] = plugin
         print(plugin + ', ', end='')
     print(co.yellow('Running'))
+
+    @t.on_process_done
+    def on_process_done(pid, data):
+        print('--', co.green(procs[pid]), co.blue('@' + str(pid)), data.getvalue().strip() if data else None)
 
     t.wait()
     print(co.yellow('Finished'))
@@ -110,36 +114,6 @@ def up():
         print(co.green(plugin), co.blue('@' + str(pid)), data.getvalue().strip())
 
     clean_unused_plugins(procs.values())
-
-def get_repo_cmd(url, dst=None, dvcs=None, cwd=''):
-
-    force_clone = False
-    if 'force-clone' in url:
-        url = url.replace('force-clone', '').strip()
-        force_clone = True
-
-    if dst is None:
-        dst = os.path.basename(url)
-        if dst.endswith('.git'):
-            dst = dst[:-4]
-
-    dst = p.abspath(p.expanduser(p.join(cwd, dst)))
-
-    if dvcs is None:
-        if 'github' in url:
-            dvcs = 'git'
-        else:
-            dvcs = 'hg'
-
-    if p.isdir(dst) and force_clone:
-        local('rm -Rf ' + dst)
-
-    if p.isdir(dst):
-        cmd = 'cd ' + dst + '&& ' + dvcs + (' fetch' if dvcs == 'hg' else ' pull')
-    else:
-        cmd = dvcs + ' clone "' + url + '"' + ('' if dst is None else ' "' + dst + '"')
-
-    return p.basename(dst), cmd
 
 def clean_unused_plugins(plugins):
     all_plugins = set(os.listdir('bundle'))
