f9bf43eabd364efed83051eaa5799444863bcd1c
Shrikant-Sharat <shrikantsharat.k@gmail.com>
2011-12-03 22:27:12 +0530

    Setup oh-my-zsh as a subrepo. Let's see how this goes.

--- fabfile.py
+++ fabfile.py
@@ -201,7 +201,7 @@ def up():
         'wget | http://releases.clojure-cake.org/cake                        | ~/bin/cake',
         'wget | https://bitbucket.org/sjl/t/raw/tip/t.py                     | ~/.t.py',
         'wget | https://github.com/sjl/z-zsh/raw/master/z.sh                 | shell/custom-configs/sjl-z.sh',
-        'repo | git clone git@github.com:sharat87/oh-my-zsh.git              | shell/oh-my-zsh',
+        # 'repo | git clone git@github.com:sharat87/oh-my-zsh.git              | shell/oh-my-zsh',
         'repo | https://github.com/zsh-users/zsh-syntax-highlighting.git     | shell/custom-configs/plugins/zsh-syntax-highlighting',
     )
 
@@ -219,9 +219,9 @@ def up():
     t.wait()
     print('Finished')
 
-    with lcd('shell/oh-my-zsh'):
-        local('rm -Rfv custom')
-        local('ln -sv ../custom-configs custom')
+    # with lcd('shell/oh-my-zsh'):
+    #     local('rm -Rfv custom')
+    #     local('ln -sv ../custom-configs custom')
 
     # VIMFILES updating
 
