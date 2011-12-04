6739591cd7f13a864ca34e4f600e52047dfd036f
Shrikant-Sharat <shrikantsharat.k@gmail.com>
2011-12-04 12:02:40 +0530

    Do away with the comments.

--- fabfile.py
+++ fabfile.py
@@ -200,8 +200,6 @@ def up():
         'wget | https://github.com/technomancy/leiningen/raw/stable/bin/lein | ~/bin/lein',
         'wget | https://bitbucket.org/sjl/t/raw/tip/t.py                     | ~/.t.py',
         'wget | https://github.com/sjl/z-zsh/raw/master/z.sh                 | shell/custom-configs/sjl-z.sh',
-        # 'repo | git clone git@github.com:sharat87/oh-my-zsh.git              | shell/oh-my-zsh',
-        # 'repo | https://github.com/zsh-users/zsh-syntax-highlighting.git     | shell/custom-configs/plugins/zsh-syntax-highlighting',
     )
 
     for entry in update_entries:
@@ -217,10 +215,6 @@ def up():
 
     t.wait()
     print('Finished')
-
-    # with lcd('shell/oh-my-zsh'):
-    #     local('rm -Rfv custom')
-    #     local('ln -sv ../custom-configs custom')
 
     # VIMFILES updating
 
